ed660e1317a25b137d9934aedf7e65811cb17258
Jack Higgins <jackhiggins07@gmail.com>
2013-12-07 21:39:37 +0000

    Initial Commit

---
+++ src/display/CreateDisplay.java
@@ -0,0 +1,45 @@
+package display;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW; 
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+
+public class CreateDisplay {
+	
+	public static void frame(){
+		try{
+			
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("CSGame");
+			Display.create();
+			Display.setResizable(true);
+		
+		
+		}catch(LWJGLException e){
+			e.printStackTrace();
+			Display.destroy();
+		}
+		
+		initGL();
+	}
+	
+	public static void initGL(){
+		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, 640, 480, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
+		
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		
+	}
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display; 
+
+public class Main {
+
+	public static void main(String [] args) {
+		display.CreateDisplay.frame();
+		System.out.println("Test");
+		
+		while(!Display.isCloseRequested()){ //game loop
+			gameLoop();
+		}
+		
+		System.exit(0);
+	}
+	
+	public static void gameLoop() {
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			System.out.println("Left");
+		}
+	}
+
+}
