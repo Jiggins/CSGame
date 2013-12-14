59d082d8d6bc6c54b4de08f81d330c264d48b366
Jack Higgins <jackhiggins07@gmail.com>
2013-12-14 15:04:33 +0000

    Added Sublime Project and Build System

--- SublimeText/LaunchGame.bat
+++ SublimeText/LaunchGame.bat
@@ -1,3 +1,5 @@
+ECHO Compiling..
 javac -d bin -sourcepath src -classpath lib/jars/lwjgl-2.9.0.jar:lib/jars/LeapJava.jar:lib/jars/slick-util.jar src/CSGame.java 
 cd bin
+ECHO Launching Game..
 java -cp :../lib/jars/LeapJava.jar:../lib/jars/lwjgl-2.9.0.jar:../lib/jars/slick-util.jar -Djava.library.path=../lib/natives/ CSGame
