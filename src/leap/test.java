2905f037b851ef2824afafdf48742b8e33dfc3cb
Jack Higgins <jackhiggins07@gmail.com>
2013-12-20 14:01:53 +0000

    Toastie Added Leap stuff!!

--- src/leap/test.java
+++ src/leap/test.java
@@ -1,9 +1,27 @@
 package leap;
 
+import com.leapmotion.leap.*;
+
 public class test {
 
 	public test() {
-		// TODO Auto-generated constructor stub
+		
+		Controller controller = new Controller();
+		
+		Frame currentFrame = new Frame();
+		
+		while(true){
+			
+			currentFrame = controller.frame();
+			
+			System.out.println(currentFrame.fingers().leftmost().tipPosition().getX());
+			
+			for(int i = 0; i<currentFrame.fingers().count();i++){
+				
+			}
+			
+		}
+		
 	}
 
 }
