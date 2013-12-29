d26b87f2c33f7f4dca1b66d141779458481f5a1e
Niamh1 <ndootson2210@hotmail.com>
2013-12-29 18:25:22 +0000

    Awesome fade out closing screen which doesn't fade out

--- src/menu/Closing.java
+++ src/menu/Closing.java
@@ -3,7 +3,6 @@ package menu;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
-
 import world.BGround;
 
 public class Closing {
@@ -13,15 +12,20 @@ public class Closing {
 	
 	public static void loop(){
 		
-		closing.loadTexture();
 		int x = 0;
 		
-		while(x < 1000 /* !start.click() , start button not pressed*/){
+		while(x < 10000 /* !start.click() , start button not pressed*/){
 			
-			GL11.glClearColor(0f, 0f, 0f, 0f);
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 			
-			closing.render();
+			GL11.glBegin(GL11.GL_QUADS);
+				
+				GL11.glColor4f(0, 0, 0, x/1000);
+				GL11.glVertex2i(0, 0); // Upper-left
+				GL11.glVertex2i(Display.getWidth(), 0); //Upper-right
+				GL11.glVertex2i(Display.getWidth(), Display.getHeight()); //Bottom-right
+				GL11.glVertex2i(0, Display.getHeight()); //Bottom-left
+			GL11.glEnd();
+			
 			Display.update();
 			x++;
 		}
