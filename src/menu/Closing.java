f6083c0921c4457dab0389ad97eb49e9feb02b10
Niamh1 <ndootson2210@hotmail.com>
2013-12-29 18:01:51 +0000

    Doesn't crash when closes

--- src/menu/Closing.java
+++ src/menu/Closing.java
@@ -14,18 +14,20 @@ public class Closing {
 	public static void loop(){
 		
 		closing.loadTexture();
+		int x = 0;
 		
-		while(true /* !start.click() , start button not pressed*/){
+		while(x < 1000 /* !start.click() , start button not pressed*/){
 			
 			GL11.glClearColor(0f, 0f, 0f, 0f);
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 			
 			closing.render();
 			Display.update();
+			x++;
 		}
 		
-		//Display.destroy();
-		//System.exit(0);
+		Display.destroy();
+		System.exit(0);
 		
 		
 	}
