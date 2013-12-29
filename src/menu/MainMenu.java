4ec3326f934c6cfced4c88c93b854fba3ea2eb41
Niamh1 <ndootson2210@hotmail.com>
2013-12-29 16:44:03 +0000

    Created closing screen

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -16,7 +16,7 @@ public class MainMenu {
 	
 	
 	public static void loop(){
-		while(true /*button not pressed*/){
+		while(true /* !start.click() , start button not pressed*/){
 			
 			GL11.glClearColor(0f, 0f, 0f, 0f);
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
