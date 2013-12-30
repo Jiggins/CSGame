0c3afb4f88f6794a3f27c2ddf25d543f783a25fb
Jack Higgins <jackhiggins07@gmail.com>
2013-12-30 10:10:21 +0000

    Cleaned up + removed comments

--- src/main/CSGame.java
+++ src/main/CSGame.java
@@ -12,23 +12,13 @@ import org.lwjgl.opengl.Display;
 import entity.Character;
 
 public class CSGame {
-	private static Character test;
-	
 	public static ArrayList<Button> buttons = new ArrayList<Button>();
 	
 	public static States state = States.Main_Menu;
 
 	public static void main(String [] args) {
 		display.CreateDisplay.frame();
-		
-		System.out.println("Hi");
-		
-//		test = new Character(0, 0, "NUIMaynoothLogo");
-//		
-//		test.setTextureLocation("NUIMaynoothLogo");
-
-//		world.ChangeBGround.changeBGround(BGround.lab3);
-		
+				
 		while(!Display.isCloseRequested()) { 
 			
 			switch(state){
@@ -48,16 +38,5 @@ public class CSGame {
 		}
 		
 		Closing.loop();
-	}	
-	
-		
-		
-//		currentFrame = controller.frame();
-//		x = currentFrame.hands().leftmost().fingers().rightmost().tipPosition().getX() + 320;
-//		y = currentFrame.hands().leftmost().fingers().rightmost().tipPosition().getZ() + 240;
-//		x2 = currentFrame.hands().rightmost().fingers().leftmost().tipPosition().getX() + 320;
-//		y2 = currentFrame.hands().rightmost().fingers().leftmost().tipPosition().getZ() + 240;
-//		System.out.println("X: " + x + " Y: " + y + " X1: " +  x2 + " Y2: " + y2);
-//		test.render(x, y, x2, y2);
-		
+	}			
 }
