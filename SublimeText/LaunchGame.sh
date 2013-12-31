e9918154cb4a9062d7ccd2e58066a29fabaf6beb
Jack Higgins <jackhiggins07@gmail.com>
2013-12-31 17:02:15 +0000

    Fixed Sublime build script for new Main method location

--- SublimeText/LaunchGame.sh
+++ SublimeText/LaunchGame.sh
@@ -1,5 +1,5 @@
 echo Compiling..
-javac -d bin -sourcepath src -classpath lib/jars/lwjgl-2.9.0.jar:lib/jars/LeapJava.jar:lib/jars/slick-util.jar src/CSGame.java 
+javac -d bin -sourcepath src -classpath lib/jars/lwjgl-2.9.0.jar:lib/jars/LeapJava.jar:lib/jars/slick-util.jar src/main/CSGame.java 
 cd bin
 echo Launching Game..
-java -cp :../lib/jars/LeapJava.jar:../lib/jars/lwjgl-2.9.0.jar:../lib/jars/slick-util.jar -Djava.library.path=../lib/natives/ CSGame
+java -cp :../lib/jars/LeapJava.jar:../lib/jars/lwjgl-2.9.0.jar:../lib/jars/slick-util.jar -Djava.library.path=../lib/natives/ main.CSGame
