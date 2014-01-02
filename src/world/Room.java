54467dcdcf10095d3091b547bcfa2dfde0395e30
Niamh1 <ndootson2210@hotmail.com>
2014-01-02 17:27:56 +0000

    Made a room object

---
+++ src/world/Room.java
@@ -0,0 +1,22 @@
+package world;
+
+import entity.Entity;
+
+public class Room {
+	
+	public Room(){
+		
+	}
+	
+	/*
+	 *  Each type of room extends Room
+	 *  
+	 *  Need:
+	 *  List of entities - arraylist
+	 *  Background
+	 *  boundaries - maybe
+	 *  
+	 *  
+	 */
+	
+}
