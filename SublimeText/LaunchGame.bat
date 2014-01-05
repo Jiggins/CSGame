d0491a1637946743a1bdabaaf9bfb44267eceb48
Jack Higgins <jackhiggins07@gmail.com>
2014-01-05 13:21:27 +0000

    Fixed Sublime Build for Windows

--- SublimeText/LaunchGame.bat
+++ SublimeText/LaunchGame.bat
@@ -1,5 +1,5 @@
 echo Compiling..
-javac -d bin -sourcepath src -classpath lib/jars/lwjgl.jar:lib/jars/lwjgl_util.jar:lib/jars/LeapJava.jar:lib/jars/slick-util.jar src/main/CSGame.java 
+javac -d bin -sourcepath src -classpath lib\jars\lwjgl.jar;lib\jars\lwjgl_util.jar;lib\jars\LeapJava.jar;lib\jars\slick-util.jar src\main\CSGame.java 
 cd bin
 echo Launching Game..
-java -cp :../lib/jars/LeapJava.jar:../lib/jars/lwjgl.jar:../lib/jars/lwjgl_util.jar:../lib/jars/slick-util.jar -Djava.library.path=../lib/natives/lwjgl:../lib/natives/Leap main.CSGame
+java -cp ;..\lib\jars\LeapJava.jar;..\lib\jars\lwjgl.jar;..\lib\jars\lwjgl_util.jar;..\lib\jars\slick-util.jar -Djava.library.path=..\lib\natives\lwjgl;..\lib\natives\Leap main.CSGame
